42d9b803e20857a6a9ebbade27725c5d8ef3f3a1
unknown <k23cb602@cnc.kyusan-u.ac.jp>
2025-04-12 18:11:23 +0900

    編集

--- js99/99.js
+++ js99/99.js
@@ -4,7 +4,7 @@ for(let x=1; x<=99; x+=1){
 }
 html += '</tr>';
 for(let y=1; y<=99; y+=1){
-    html +='<tr><th>' +x*y+'</th>';
+    html +='<tr><th>' +y+'</th>';
     for(let x=1; x<=99; x+=1){
         html += '<td>'+ x*y + '</td>';
     } 
