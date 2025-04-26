ecf4e1d8c8ea8061bac2c8e5b581a2f99a46f078
unknown <k23cb602@cnc.kyusan-u.ac.jp>
2025-04-26 14:37:00 +0900

    修正

--- jset/jsetc1.js
+++ jset/jsetc1.js
@@ -6,6 +6,7 @@ btn.addEventLisntener('click',() => {
      if(isNaN(inp.value)){
          elem.innerText = "<半角英数字で数字を入れてください>";
      } else{
-        elem.innerText = inp.value * 1.10;
+         let result = inp.value * 1.10;
+        elem.innerText = Math.roumd(result);
      }
 } );
