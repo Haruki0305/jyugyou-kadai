86598c4a07263d1857d71a8a86ca239e25e8d2e3
unknown <k23cb602@cnc.kyusan-u.ac.jp>
2025-04-26 11:22:32 +0900

    修正

--- jsadv/advmap.js
+++ jsadv/advmap.js
@@ -1,7 +1,7 @@
-let showmap = () => {
+let showMap = () => {
     let elem = document.querySelector('div')
     let chip =map[y][x];
     elem.innerHTML ='<img scr="adv_img/' + imges[chip] + '.pmg"><img scr="adv_img/man.png">';
 };
 
-showmap();
+showMap();
