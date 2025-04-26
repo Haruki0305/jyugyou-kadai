41764812e47e29e74b03ec9ceaadf412f0e2b763
unknown <k23cb602@cnc.kyusan-u.ac.jp>
2025-04-26 14:49:07 +0900

    新規

--- jset/jsetc1.js
+++ jset/jsetc1.js
@@ -4,7 +4,7 @@ let elem  = document.querySelector('p2');
 
 btn.addEventLisntener('click',() =>{
      if(isNaN(inp.value)){
-         elem.innerText = "<半角英数字で数字を入れてください>";
+         elem.innerText = "＜半角英数字で数字を入れてください＞";
      } else{
          let result = inp.value * 1.10;
          elem.innerText = Math.roumd(result);
